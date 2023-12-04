c517cb59d1d839d3b61c846ebed3406054c87d6c
Jose RM <mojica.raul@gmail.com>
2023-12-04 16:06:56 -0600

    Add can_be_archived and move_to_archive methods

--- item.rb
+++ item.rb
@@ -1,3 +1,5 @@
+requiere 'date'
+
 class Item
   attr_accessor :genre, :author, :source, :label, :publish_date
 
@@ -5,5 +7,17 @@ class Item
     @id = Random.rand(1..10_000)
     @publish_date = publish_date
     @archived = false
+  end
+
+  private
+
+  def can_be_archived?
+    publish_year = @publish_date.year
+    total_years = Date.year - publish_year
+    total_years > 10
+  end
+
+  def move_to_archive()
+    @archived = true if can_be_archived?
   end
 end
