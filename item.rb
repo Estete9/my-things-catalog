e50c8f81a073fdf92f7f6b1f9a97f1cdf5208379
Esteban <cepalacios@outlook.com>
2023-12-04 20:26:21 -0500

    TDD implement book class and tests

--- item.rb
+++ item.rb
@@ -10,16 +10,17 @@ class Item
     @archived = false
   end
 
+  def move_to_archive()
+    @archived = true if can_be_archived?
+    false
+  end
+
   private
 
   def can_be_archived?
     publish_year = @publish_date.year
-    total_years = Date.year - publish_year
+    current_year = Date.today.year
+    total_years = current_year - publish_year
     total_years > 10
-  end
-
-  def move_to_archive()
-    @archived = true if can_be_archived?
-    false
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,10 +1,11 @@
 require_relative 'spec_helper'
+require 'date'
 
 describe Book do
   before :each do
     publisher = 'Penguin'
-    cover_state = 'New'
-    publish_date = '2015-12-12'
+    cover_state = 'new'
+    publish_date = Date.new(2005, 12, 12)
 
     @book = Book.new(publisher, cover_state, publish_date)
   end
@@ -12,6 +13,25 @@ describe Book do
   context 'Inheritance from Item' do
     it 'confirms that the Book class inherits from Item' do
       expect(@book).to be_a(Item)
+    end
+  end
+
+  context '#can_be_archived?' do
+    it 'returns false if the cover state is not bad' do
+      expect(@book.can_be_archived?).to be(false)
+    end
+
+    it 'returns false if the date of publication is newer than 10 years' do
+      @book.cover_state = 'bad'
+      @book.publish_date = Date.new(2015, 12, 12)
+
+      expect(@book.can_be_archived?).to be(false)
+    end
+
+    it 'returns true if the date of publication is older than 10 years AND the cover_state is bad' do
+      @book.cover_state = 'bad'
+
+      expect(@book.can_be_archived?).to be(true)
     end
   end
 end
