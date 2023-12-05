e1f665380b1f729d237de602380d3ec216321e7d
Esteban <cepalacios@outlook.com>
2023-12-05 06:45:12 -0500

    Add tests and properties for genre author and label in the book class

--- book.rb
+++ book.rb
@@ -3,10 +3,13 @@ require_relative 'item'
 class Book < Item
   attr_accessor :cover_state
 
-  def initialize(publisher, cover_state, publish_date)
+  def initialize(publisher, cover_state, publish_date, genre, author, label)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
+    @genre = genre
+    @author = author
+    @label = label
   end
 
   def can_be_archived?
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,8 +6,27 @@ describe Book do
     publisher = 'Penguin'
     cover_state = 'new'
     publish_date = Date.new(2005, 12, 12)
+    @genre = instance_double('Genre', name: 'Science Fiction')
+    @author = instance_double('Author', first_name: 'Douglas', last_name: 'Adams')
+    @label = instance_double('Label', title: 'Gift', color: 'Black')
 
-    @book = Book.new(publisher, cover_state, publish_date)
+    @book = Book.new(publisher, cover_state, publish_date, @genre, @author, @label)
+  end
+
+  context '#initialize' do
+    it 'sets the genre, author, and label in the inherited properties' do
+      genre_name = 'Science Fiction'
+      author_f_name = 'Douglas'
+      author_l_name = 'Adams'
+      label_title = 'Gift'
+      lable_color = 'Black'
+
+      expect(@book.genre.name).to eql(genre_name)
+      expect(@book.author.first_name).to eql(author_f_name)
+      expect(@book.author.last_name).to eql(author_l_name)
+      expect(@book.label.title).to eql(label_title)
+      expect(@book.label.color).to eql(lable_color)
+    end
   end
 
   context 'Inheritance from Item' do
