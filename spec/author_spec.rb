c82cb59ef6f2c73ddb6030cd75006fa5a72bf58d
Jose RM <mojica.raul@gmail.com>
2023-12-05 16:54:46 -0600

    Create the testing file author_spec.rb and add one line in the spec_helper.rb file

---
+++ spec/author_spec.rb
@@ -0,0 +1,36 @@
+require_relative 'spec_helper'
+
+describe Author do
+  let(:author) { Author.new('John', 'Doe') }
+  let(:item) { double('item') }
+
+  describe '#initialize' do
+    it 'sets a random id' do
+      expect(author.id).to be_a(Integer)
+    end
+
+    it 'sets the first name' do
+      expect(author.first_name).to eq('John')
+    end
+
+    it 'sets the last name' do
+      expect(author.last_name).to eq('Doe')
+    end
+
+    it 'initializes an empty items array' do
+      expect(author.items).to eq([])
+    end
+  end
+
+  describe '#add_item' do
+    it 'adds an item to the items array' do
+      author.add_item(item)
+      expect(author.items).to include(item)
+    end
+
+    it 'calls add_author on the item' do
+      expect(item).to receive(:add_author).with(author)
+      author.add_item(item)
+    end
+  end
+end
