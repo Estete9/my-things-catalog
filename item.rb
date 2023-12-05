08543de002ce492f19d3ea7ef4b3186f319d673f
Erwin Amador <eamador0305@gmail.com>
2023-12-04 20:07:17 -0600

    method from private to public

--- item.rb
+++ item.rb
@@ -9,15 +9,15 @@ class Item
     @archived = false
   end
 
+  def move_to_archive()
+    @archived = true if can_be_archived?
+  end
+
   private
 
   def can_be_archived?
     publish_year = @publish_date.year
     total_years = Date.year - publish_year
     total_years > 10
-  end
-
-  def move_to_archive()
-    @archived = true if can_be_archived?
   end
 end
