2075675d28cee35be3fdf85b00dbd12d1773b4a3
Esteban <cepalacios@outlook.com>
2023-12-05 07:29:37 -0500

    Implement label class with TDD and relationship one to many

---
+++ label.rb
@@ -0,0 +1,14 @@
+class Label
+  attr_accessor :title, :color
+  attr_reader :items
+
+  def initialize(title, color)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
---
+++ spec/label_spec.rb
@@ -0,0 +1,52 @@
+require_relative 'spec_helper'
+
+describe Label do
+  before :each do
+    title = 'Gift'
+    color = 'Black'
+    @label = Label.new(title, color)
+  end
+
+  context '#title' do
+    it 'gets the title of the label' do
+      label_title = 'Gift'
+
+      expect(@label.title).to eql(label_title)
+    end
+
+    it 'sets the title of the label' do
+      new_title = 'New'
+
+      @label.title = new_title
+
+      expect(@label.title).to eql(new_title)
+    end
+  end
+
+  context '#color' do
+    it 'gets the color of the label' do
+      label_color = 'Black'
+
+      expect(@label.color).to eql(label_color)
+    end
+
+    it 'sets the color of the label' do
+      new_color = 'White'
+
+      @label.color = new_color
+
+      expect(@label.color).to eql(new_color)
+    end
+  end
+
+  context '#add_item' do
+    it 'adds an Item object to the items array property' do
+      publish_date = '2020-12-12'
+      item = instance_double(Item, publish_date: publish_date)
+
+      @label.add_item(item)
+
+      expect(@label.items.length).to eql(1)
+    end
+  end
+end
