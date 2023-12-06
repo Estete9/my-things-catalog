ac76928502a08627f1264d6388d6c75b6e105181
Jose RM <mojica.raul@gmail.com>
2023-12-06 10:24:04 -0600

    Add the methods for displaying the info about games and the authorr

--- display_module.rb
+++ display_module.rb
@@ -24,6 +24,29 @@ module DisplayModule
     end
   end
 
+  def list_all_authors
+    @authors.each { |author| puts s_to_capital(author) }
+  end
+
+  def list_all_games
+    @games.each do |game|
+      author_f_name = s_to_capital(game.author.first_name)
+      author_l_name = s_to_capital(game.author.last_name)
+      genre = s_to_capital(game.genre.name)
+      label_title = s_to_capital(game.label.title)
+      label_color = s_to_capital(game.label.color)
+
+      details = <<~DETAILS
+        Author: #{author_f_name} #{author_l_name}
+        Genre: #{genre}
+        Label title: #{label_title}, Label color: #{label_color}
+        Publisher: #{game.publisher}
+        Id: #{game.id}
+      DETAILS
+      puts details
+    end
+  end
+
   private
 
   def s_to_capital(item)
