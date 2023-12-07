3fd174c90300826c513d903ca8bf4d91d330b2c0
Erwin Amador <eamador0305@gmail.com>
2023-12-07 01:00:55 -0600

    change the arguments in MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -3,11 +3,8 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(genre, author, label, publish_date, on_spotify)
+  def initialize(on_spotify, publish_date)
     super(publish_date)
-    @genre = genre
-    @author = author
-    @label = label
     @on_spotify = on_spotify
   end
 
