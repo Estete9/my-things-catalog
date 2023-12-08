a8aa2dcc0fede652b66dc3736ad53092f893dc44
Erwin Amador <eamador0305@gmail.com>
2023-12-07 21:20:11 -0600

    Update game.rb

--- game.rb
+++ game.rb
@@ -19,7 +19,7 @@ class Game < Item
     super && Time.new.year - last_played_at > 2 unless last_played_at.nil?
   end
 
-  def to_h
+  def to_h(except: false)
     super.merge('multiplayer' => multiplayer, 'last_played_at' => last_played_at)
   end
 end
